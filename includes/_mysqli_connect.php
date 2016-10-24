86c38c5b00a6a6c8fb9a1247a81d8e77df767ea0
coolKIH <caozihuahh@gmail.com>
2016-10-24 18:05:04 +0800

    Database notes

---
+++ includes/_mysqli_connect.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hao
+ * Date: 9/9/16
+ * Time: 7:04 PM
+ */
+DEFINE ('DB_USER','yourusername');
+DEFINE ('DB_PWD','yourpsw');
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_NAME','WebDb');
+
+$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PWD,DB_NAME) OR die('Could not connect to MYSQL:'.mysqli_connect_error());
+mysqli_query($dbc,'set names utf8');
+?>
